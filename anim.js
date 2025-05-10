c7c35726a980e8988655ca8e475041aef12ca1b1
hunson <hunson89123@gmail.com>
2025-05-10 13:22:29 +0800

    feat: 新增第二頁中英文字體，移除開始動畫，調整英文名、專長與座右銘文字換行

--- anim.js
+++ anim.js
@@ -3,61 +3,59 @@ $(document).ready(function () {
         "#erin-logo",
         {
             keyframes: [
-                { opacity: 1, duration: .5, ease: "circ.in" },
-                { opacity: 1, duration: 2 },
-                { left: 0, top: 0, x: "30%", y: "30%", scale: .5, duration: 1, ease: "circ.out" }
+                { opacity: 1, duration: .5, ease: "circ.in" }
             ]
         }
     );
 
-    gsap.to(
-        ".carousel_cover",
-        {
-            keyframes: [
-                { opacity: 1, duration: 2.5, },
-                { opacity: .65, duration: 1, ease: "circ.out" }
-            ]
-        }
-    );
+    // gsap.to(
+    //     ".carousel_cover",
+    //     {
+    //         keyframes: [
+    //             { opacity: 1, duration: 2.5, },
+    //             { opacity: .65, duration: 1, ease: "circ.out" }
+    //         ]
+    //     }
+    // );
 
     gsap.fromTo(
         "#logo-06",
         { scale: 0, opacity: 0, rotation: -90 },
-        { scale: 1, opacity: 1, rotation: 0, duration: 1, delay: 3, ease: "back.out" }
+        { scale: .85, opacity: 1, rotation: 0, duration: 1, ease: "back.out" }
     );
 
     gsap.fromTo(
         "#logo-07",
         { scale: 0, opacity: 0, rotation: -90 },
-        { scale: 1, opacity: 1, rotation: 0, duration: 1, delay: 3, ease: "back.out" }
+        { scale: .85, opacity: 1, rotation: 0, duration: 1, ease: "back.out" }
     );
 
     gsap.fromTo(
         "#logo-08",
         { scale: 0, opacity: 0, rotation: -90 },
-        { scale: 1, opacity: 1, rotation: 0, duration: 1, delay: 3, ease: "back.out" }
+        { scale: .85, opacity: 1, rotation: 0, duration: 1, ease: "back.out" }
     );
 
     gsap.fromTo(
         "#logo-09",
         { scale: 0, opacity: 0, rotation: -90 },
-        { scale: 1, opacity: 1, rotation: 0, duration: 1, delay: 3, ease: "back.out" }
+        { scale: .85, opacity: 1, rotation: 0, duration: 1, ease: "back.out" }
     );
 
     gsap.fromTo(
         "#cross",
         { scale: 0, opacity: 0 },
-        { scale: 1, opacity: 1, delay: 3, duration: 1 }
+        { scale: 1, opacity: 1, duration: 1 }
     );
 
     gsap.fromTo(
         "#title",
         { y: "0" },
-        { y: 0, opacity: 1, delay: 3, duration: 1 }
+        { y: 0, opacity: 1, duration: 1 }
     );
 
-    gsap.delayedCall(4, () => {
-        document.getElementById("section1").style.display = "flex";
-        document.getElementById("section1").style.height = "100dvh";
-    });
+    // gsap.delayedCall(4, () => {
+    //     document.getElementById("section1").style.display = "flex";
+    //     document.getElementById("section1").style.height = "100dvh";
+    // });
 });
